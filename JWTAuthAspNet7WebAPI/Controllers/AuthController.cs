de95c526efc4895bd7e1e45d3ddb103b82a16250
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2024-02-15 19:27:29 +0700

    refactor entrie api to use ApplicationUser

--- JWTAuthAspNet7WebAPI/Controllers/AuthController.cs
+++ JWTAuthAspNet7WebAPI/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using JWTAuthAspNet7WebAPI.Core.Dtos;
+using JWTAuthAspNet7WebAPI.Core.Entities;
 using JWTAuthAspNet7WebAPI.Core.OtherObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -14,10 +15,10 @@ namespace JWTAuthAspNet7WebAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IConfiguration _configuration;
-        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        public AuthController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _roleManager = roleManager;
@@ -57,8 +58,10 @@ namespace JWTAuthAspNet7WebAPI.Controllers
                 return BadRequest("UserName Already Exist");
             }
 
-            IdentityUser newUser = new IdentityUser()
+            ApplicationUser newUser = new ApplicationUser()
             {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
                 Email = registerDto.Email,
                 UserName = registerDto.UserName,
                 SecurityStamp = Guid.NewGuid().ToString(),
@@ -104,6 +107,8 @@ namespace JWTAuthAspNet7WebAPI.Controllers
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.NameIdentifier, user.Id),
                 new Claim("JWTID", Guid.NewGuid().ToString()),
+                new Claim("FirstName", user.FirstName),
+                new Claim("LastName", user.LastName),
             };
 
             foreach(var userRole in userRoles)
--- JWTAuthAspNet7WebAPI/Program.cs
+++ JWTAuthAspNet7WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using JWTAuthAspNet7WebAPI.Core.DBContext;
+using JWTAuthAspNet7WebAPI.Core.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -23,7 +24,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
 
 //Add Identity
 builder.Services
-    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDBContext>()
     .AddDefaultTokenProviders();
 
